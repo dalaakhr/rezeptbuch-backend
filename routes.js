06ff35febd35ebcd84455673c7c0a47a682f7276
Dalaa Khreis <288004092+dalaakhr@users.noreply.github.com>
2026-09-21 13:38:34 +0200

    POST und PATCH um Zutaten und Zubereitung erweitert

--- routes.js
+++ routes.js
@@ -3,17 +3,19 @@ const router = express.Router();
 const Rezept = require('./models/rezepte');
 
 // alle Rezepte abrufen
-router.get('/rezepte', async(req, res) => {
+router.get('/rezepte', async (req, res) => {
     const alleRezepte = await Rezept.find();
     res.send(alleRezepte);
 });
 
 // ein neues Rezept anlegen
-router.post('/rezepte', async(req, res) => {
+router.post('/rezepte', async (req, res) => {
     const neuesRezept = new Rezept({
         titel: req.body.titel,
         kategorie: req.body.kategorie,
         zeit: req.body.zeit,
+        zutaten: req.body.zutaten,
+        zubereitung: req.body.zubereitung,
         gemacht: false
     });
     await neuesRezept.save();
@@ -21,26 +23,28 @@ router.post('/rezepte', async(req, res) => {
 });
 
 //ein Rezept ändern
-router.patch('/rezepte/:id', async(req, res) =>{
+router.patch('/rezepte/:id', async (req, res) => {
     try {
         const rezept = await Rezept.findOne({ _id: req.params.id });
 
-            if (req.body.titel) rezept.titel = req.body.titel;
-            if (req.body.kategorie) rezept.kategorie = req.body.kategorie;
-            if (req.body.zeit) rezept.zeit = req.body.zeit;
-            if (req.body.gemacht !== undefined) rezept.gemacht = req.body.gemacht;
+        if (req.body.titel) rezept.titel = req.body.titel;
+        if (req.body.kategorie) rezept.kategorie = req.body.kategorie;
+        if (req.body.zeit) rezept.zeit = req.body.zeit;
+        if (req.body.gemacht !== undefined) rezept.gemacht = req.body.gemacht;
+        if (req.body.zutaten) rezept.zutaten = req.body.zutaten;
+        if (req.body.zubereitung) rezept.zubereitung = req.body.zubereitung;
 
-            await Rezept.updateOne({ _id: req.params.id }, rezept);
-            res.send(rezept);
-        } catch {
+        await Rezept.updateOne({ _id: req.params.id }, rezept);
+        res.send(rezept);
+    } catch {
         res.status(404);
         res.send({ error: "Rezept existiert nicht!" });
-        }
-    
+    }
+
 });
 
 //ein Rezept löschen
-router.delete('/rezepte/:id', async(req, res) => { 
+router.delete('/rezepte/:id', async (req, res) => {
     try {
         await Rezept.deleteOne({ _id: req.params.id });
         res.status(204);
